3b5012458c2f913ee5bdfb64e443a6760a65fcaa
Christoph Weitkamp <mail@christophweitkamp.de>
2017-02-14 19:21:41 +0100

    added additional xml-element tags for creating objects by JAXBContext

--- addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/ahamodel/DeviceModel.java
+++ addons/binding/org.openhab.binding.avmfritz/src/main/java/org/openhab/binding/avmfritz/internal/ahamodel/DeviceModel.java
@@ -61,10 +61,13 @@ public class DeviceModel {
 	@XmlElement(name = "name")
 	private String name;
 	
+	@XmlElement(name = "switch")
 	private SwitchModel switchModel;
 	
+	@XmlElement(name = "powermeter")
 	private PowerMeterModel powermeterModel;
 
+	@XmlElement(name = "temperature")
 	private TemperatureModel temperatureModel;
 
 	public PowerMeterModel getPowermeter() {
